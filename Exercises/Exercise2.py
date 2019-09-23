52940174b04bf938ead04f7fe1fba5b36eef954a
Sivaharan Thevipagan <SThevipagan@spartaglobal.com>
2019-09-23 14:20:05 +0100

    second exercise completed

---
+++ Exercises/Exercise2.py
@@ -0,0 +1,19 @@
+# I want you to use operators
+# Equate something
+
+print(2 == 3)
+
+# As a user, I want to be able to guess a number and know if I got it, correct or not, so that I can know if I won or not.
+
+# We should define/assign a number to a variable called magic_number.
+
+Magic_number = 26
+
+# I need to ask user for input.
+
+Magic_number1 = int(input('Pick a number'))
+
+# I need to check if this input matches a magic_number.
+print('Your guess was: ' + str(Magic_number == Magic_number1))
+print('Your guess was:', Magic_number == Magic_number1)
+# I need to let the user know if the response was right or not.
